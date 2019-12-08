8d534be933d12ebe599b7f2018bc2195867b959b
tirta <tirtajiwanggataufiq@gmail.com>
2019-12-08 09:06:34 +0700

    web route prefix

--- apps/modules/penilaian/config/routes/web.php
+++ apps/modules/penilaian/config/routes/web.php
@@ -2,49 +2,56 @@
 
 $namespace = "Siakad\Penilaian\Controllers\Web";
 
-$router->addGet("/listkelas",[
+$isMikel = true;
+if($isMikel){
+    $prefix = "/penilaian";
+} else {
+    $prefix = "";
+}
+
+$router->addGet("$prefix/listkelas",[
     'namespace' => $namespace,
     'module' => 'penilaian',
     'controller' => 'penilaian',
     'action' => 'listKelas'
 ]);
 
-$router->addGet("/komponenpenilaiankelas", [
+$router->addGet("$prefix/komponenpenilaiankelas", [
     'namespace' => $namespace,
     'module' => 'penilaian',
     'controller' => 'penilaian',
     'action' => 'komponenpenilaiankelas'
 ]);
 
-$router->addPost("/komponenpenilaiankelas", [
+$router->addPost("$prefix/komponenpenilaiankelas", [
     'namespace' => $namespace,
     'module' => 'penilaian',
     'controller' => 'penilaian',
     'action' => 'ubahkomponenpenilaiankelas'
 ]);
 
-$router->addGet("/lihatnilaikelas", [
+$router->addGet("$prefix/lihatnilaikelas", [
     'namespace' => $namespace,
     'module' => 'penilaian',
     'controller' => 'penilaian',
     'action' => 'lihatnilaikelas'
 ]);
 
-$router->addPost("/lihatnilaikelas", [
+$router->addPost("$prefix/lihatnilaikelas", [
     'namespace' => $namespace,
     'module' => 'penilaian',
     'controller' => 'penilaian',
     'action' => 'ubahnilaikelas'
 ]);
 
-$router->addGet("/lihattranskripmahasiswa", [
+$router->addGet("$prefix/lihattranskripmahasiswa", [
     'namespace' => $namespace,
     'module' => 'penilaian',
     'controller' => 'penilaian',
     'action' => 'lihattranskripmahasiswa'
 ]);
 
-$router->addPost("/???", [
+$router->addPost("$prefix/???", [
     'namespace' => $namespace,
     'module' => 'penilaian',
     'controller' => 'penilaian',
