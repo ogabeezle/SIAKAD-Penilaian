c9f99bf7b9a37d29eae0f9c98d809e941002c313
rahadiankp <rahadian.k.putra@gmail.com>
2019-12-09 13:40:48 +0700

    add exceptions to loader

--- apps/config/loader.php
+++ apps/config/loader.php
@@ -9,9 +9,15 @@ $loader->registerNamespaces(array(
 	/**
 	 * Load SQL server db adapter namespace
 	 */
-	//'Phalcon\Db\Adapter\Pdo' => APP_PATH . '/lib/Phalcon/Db/Adapter/Pdo',
-	//'Phalcon\Db\Dialect' => APP_PATH . '/lib/Phalcon/Db/Dialect',
-	//'Phalcon\Db\Result' => APP_PATH . '/lib/Phalcon/Db/Result',
+	'Phalcon\Db\Adapter\Pdo' => APP_PATH . '/lib/Phalcon/Db/Adapter/Pdo',
+	'Phalcon\Db\Dialect' => APP_PATH . '/lib/Phalcon/Db/Dialect',
+	'Phalcon\Db\Result' => APP_PATH . '/lib/Phalcon/Db/Result',
+
+
+    /**
+     * Load common classes
+     */
+    'Siakad\Common\Exception' => APP_PATH . '/common/exception',
 
 ));
 
